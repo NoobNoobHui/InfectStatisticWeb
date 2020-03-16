481613c674b48f3d2bc89cc96f8876cd016dfcf2
damagedmj <61041545+damagedmj@users.noreply.github.com>
2020-03-16 12:36:41 +0800

    Update Province.java

--- src/infectstatistic/pojo/Province.java
+++ src/infectstatistic/pojo/Province.java
@@ -14,5 +14,63 @@ public class Province {
         this.cure = 0;
         this.dead = 0;
     }
-    
+    public Province(String name, int ip, int sp, int cure, int dead){
+        this.name = name;
+        this.ip = ip;
+        this.sp = sp;
+        this.cure = cure;
+        this.dead = dead;
+    }
+    public static Province init(){
+        return new Province(null,0,0,0,0);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setIp(int ip) {
+        this.ip = ip;
+    }
+
+    public int getIp() {
+        return ip;
+    }
+
+    public void setSp(int sp) {
+        this.sp = sp;
+    }
+
+    public int getSp() {
+        return sp;
+    }
+
+    public void setCure(int cure) {
+        this.cure = cure;
+    }
+
+    public int getCure() {
+        return cure;
+    }
+
+    public void setDead(int dead) {
+        this.dead = dead;
+    }
+
+    public int getDead() {
+        return dead;
+    }
+
 }
